59048cb277a12d8f6067260dee7d8b9218b39f64
Marcus Johansson <marcus@intertainment.tv>
2013-05-16 09:49:58 +0200

    Clean build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberDev::Application.config.secret_token = 'ab5791c5fd909aef9d8622201aefffd4704ca29e26296052319b5214fe7bdfa7a33b77c49f90f430f5f7a3cbb6d292ef45e5772c87879e35a06f7438d187bb65'
